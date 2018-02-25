4ed958572d64cd682caf2921e921a4dce7f1d725
Vladimir Yartsev <vovayartsev@gmail.com>
2018-02-25 23:38:27 +0400

    Distillery

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :terraformer, TerraformerWeb.Endpoint,
   load_from_system_env: true,
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :terraformer, Terraformer.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 1 # Free tier db only allows 1 connection
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,7 +67,3 @@ config :logger, level: :info
 #
 #     config :terraformer, TerraformerWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Terraformer.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:distillery, "~> 1.5"},
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
