4034a887ea32c898280628df874925dd1784f025
Vladimir Yartsev <vovayartsev@gmail.com>
2018-03-10 18:31:01 +0400

    Commenting the line and breaking the tests

--- lib/terraformer/heroku/projectors/addon.ex
+++ lib/terraformer/heroku/projectors/addon.ex
@@ -8,7 +8,7 @@ defmodule Terraformer.Heroku.Projectors.Addon do
 
   project %AddonProvisioned{} = provisioned do
     # IO.puts "PROJECTING: #{inspect(provisioned)}"
-    Terraformer.Repo.insert!(%Addon{uuid: provisioned.uuid})
+    # Terraformer.Repo.insert!(%Addon{uuid: provisioned.uuid})   <---- Uncommenting this fixes the tests
     Ecto.Multi.insert(multi, :addon, %Addon{uuid: provisioned.uuid})
   end
 end
